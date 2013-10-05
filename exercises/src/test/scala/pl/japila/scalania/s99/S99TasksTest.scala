d4ae257dd44b0659fe13df214260562aee105cd7
Jacek Kolodziejski <jacek.kolo@gmail.com>
2013-10-05 17:45:23 +0200

    Add signature and test implementation for p27b

--- exercises/src/test/scala/pl/japila/scalania/s99/S99TasksTest.scala
+++ exercises/src/test/scala/pl/japila/scalania/s99/S99TasksTest.scala
@@ -130,6 +130,13 @@ class S99TasksTest extends mutable.Specification {
       output.distinct.length mustEqual 1260
       output.length mustEqual 1260
     }
+    "Group 3 elements of a set into disjoint subsets of 2 and 1 elements. (generalized version)" in {
+      val inputGroups = List(2,1)
+      val inputElements = List.range(0,3)
+      val expected = List(List(List(0, 1), List(2)), List(List(0, 2) , List(1)), List(List(1, 2), List(0)))
+      solution.p27b(inputGroups, inputElements) must listMatch(expected)
+    }
+    
     " Sorting a list of lists according to length of sublists." in {
       solution.p28(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) must listMatch(  List(List('o), List('d, 'e), List('d, 'e), List('m, 'n), List('a, 'b, 'c), List('f, 'g, 'h), List('i, 'j, 'k, 'l)) )
     }
