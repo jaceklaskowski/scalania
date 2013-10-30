d4b2f1b7f1c048e1e389db61b26d3398540541b2
Jacek Laskowski <jacek@japila.pl>
2013-10-30 23:39:51 +0100

    P19Spec - new tests

--- exercises/src/test/scala/pl/japila/scalania/s99/P19Spec.scala
+++ exercises/src/test/scala/pl/japila/scalania/s99/P19Spec.scala
@@ -5,19 +5,25 @@ import S99_P19.rotate
 
 class P19Spec extends mutable.Specification {
   "P19 solution" should {
-    "Rotate a list N places to the left." in {
-      {
-        val actual = rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
-        actual === List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c)
-      }
-      {
-        val actual = rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
-        actual === List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i)
-      }
-      {
-        val actual = rotate(0, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
-        actual === List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
-      }
+    "Rotate a list 3 places to the left." in {
+      val actual = rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+      actual === List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c)
+    }
+    "Rotate a list 2 places to the right." in {
+      val actual = rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+      actual === List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i)
+    }
+    "Rotate a list 0 places to the left." in {
+      val actual = rotate(0, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+      actual === List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+    }
+    "Rotate a 2-element list 100 places to the left." in {
+      val actual = rotate(100, List('a, 'b))
+      actual === List('a, 'b)
+    }
+    "Rotate a 0-element list 3 places to the right." in {
+      val actual = rotate(-3, List())
+      actual === List()
     }
   }
 }
