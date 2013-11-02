8a543d7abd1386ff09446361bc9c1cc4cf98cab1
etorreborre <etorreborre@yahoo.com>
2013-11-02 11:24:26 +1100

    creating one example per solution

--- exercises/src/test/scala/pl/japila/scalania/euler/P01Spec.scala
+++ exercises/src/test/scala/pl/japila/scalania/euler/P01Spec.scala
@@ -5,6 +5,12 @@ import Euler_P01._
 
 class P01Spec extends Specification {
   "P01 solution" should {
+    "Find the sum of all the multiples of 3 or 5 below 1000." >> {
+      solutions2.rows.foreach { row =>
+        row.t1 >> { row.t2(3, 5, 1000) must_== 233168 }
+      }; br
+    }
+
     "Find the sum of all the multiples of 3 or 5 below 1000." in {
       solutions2 |> {
         case (name, f) =>
