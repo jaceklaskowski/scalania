31a8d9712d5a363cc2ad3105443576df4d5f4831
etorreborre <etorreborre@yahoo.com>
2013-11-04 11:09:43 +1100

    removed the use of DataTables to create multiple examples and improved the function names

--- exercises/src/test/scala/pl/japila/scalania/euler/P01Spec.scala
+++ exercises/src/test/scala/pl/japila/scalania/euler/P01Spec.scala
@@ -1,23 +1,17 @@
 package pl.japila.scalania.euler
 
 import org.specs2.mutable._
-import org.specs2.specification.Tables
 import Euler_P01._
 
-class P01Spec extends Specification with Tables with ExamplesBlock {
+class P01Spec extends Specification with ExamplesBlock {
   "P01 solution" should {
-    "Find the sum of all the multiples of 3 or 5 below 1000." >> {
-
-      val titles = List("name", "function")
-      val dr2s = List[DataRow2[String, (Int, Int, Int) => Int]]()
-      val rows = solutions.foldLeft(dr2s) { (l, f) =>
-        DataRow2(s"${f.getClass.getSimpleName} solution", f) :: l
-      }
-
-      val t2 = new Table2[String, (Int, Int, Int) => Int](titles, rows.reverse)
-      t2.rows.foreach { row =>
-        row.t1 >> { row.t2(3, 5, 1000) must_== 233168 }
+    "Find the sum of all the multiples of 3 or 5 below 1000." in {
+      solutions.map(s => (s"${solutionName(s)} solution", s)).foreach {
+        case (solution, s) =>
+          solution >> { s(3, 5, 1000) must_== 233168 }
       }
     }
   }
+
+  def solutionName(s: Any) = s.getClass.getSimpleName.split("\\$").drop(1).dropRight(1).mkString("_")
 }
