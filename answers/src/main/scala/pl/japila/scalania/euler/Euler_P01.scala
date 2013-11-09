a4bde304375aca1b67d7a147796af2fdb93888b4
Jacek Laskowski <jacek@japila.pl>
2013-11-09 20:00:03 +0100

    alternative solutions should be in the answers project

--- answers/src/main/scala/pl/japila/scalania/euler/Euler_P01.scala
+++ answers/src/main/scala/pl/japila/scalania/euler/Euler_P01.scala
@@ -1,6 +1,11 @@
 package pl.japila.scalania.euler
 
 object Euler_P01 {
+  val solutions = List[(Int, Int, Int) => Int](
+    findSumOfMultiplies
+  )
+
   def findSumOfMultiplies(a: Int = 3, b: Int = 5, limit: Int = 1000): Int =
     (0 until limit).filter(n => n % a == 0 || n % b == 0).sum
+
 }
--- exercises/src/main/scala/pl/japila/scalania/euler/Euler_P01.scala
+++ exercises/src/main/scala/pl/japila/scalania/euler/Euler_P01.scala
@@ -2,17 +2,6 @@ package pl.japila.scalania.euler
 
 object Euler_P01 {
 
-  val solutions = List[(Int, Int, Int) => Int](
-    findSumOfMultiplies,
-    findSumOfMultiplies_jl,
-    findSumOfMultiplies_eric
-  )
-
   def findSumOfMultiplies: (Int, Int, Int) => Int = (a: Int, b: Int, limit: Int) => ???
-
-  def findSumOfMultiplies_jl: (Int, Int, Int) => Int = (a: Int, b: Int, limit: Int) => ???
-
-  def findSumOfMultiplies_eric = (a: Int, b: Int, limit: Int) =>
-    (0 until limit).filter(n => n % a == 0 || n % b == 0).sum
 
 }
