09f506f6eccf908c770865a05579a81afdb189a3
Jacek Kolodziejski <jacek.kolo@gmail.com>
2013-11-30 23:23:57 +0100

    Example of tests on many implementations (P23)

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P23.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P23.scala
@@ -1,5 +1,9 @@
 package pl.japila.scalania.s99
 
 object S99_P23 {
+  def solutions[T](): List[(String, (Int, Seq[T]) => Seq[T])] = List(
+    ("user function", randomSelect)
+  )
+
   def randomSelect[T](count: Int, ts: Seq[T]): Seq[T] = ???
 }
