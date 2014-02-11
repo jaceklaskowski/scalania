12d7da80f59fe5eb8998f9db885743d0d5aeaec2
Jacek Kolodziejski <jacek.kolo@gmail.com>
2014-02-11 01:22:42 +0100

    Fix: list compare in test P27

--- exercises/src/test/scala/pl/japila/scalania/s99/P27Spec.scala
+++ exercises/src/test/scala/pl/japila/scalania/s99/P27Spec.scala
@@ -16,10 +16,13 @@ class P27Spec extends Specification {
   }
   "P27b solution" should {
     "Group 3 elements of a set into disjoint subsets of 2 and 1 elements. (generalized version)" in {
+      def toNestedSet[T](tsss: Seq[Seq[Seq[T]]]): Set[Set[Set[T]]] =
+        tsss.map(_.map(_.toSet).toSet).toSet
+
       val inputGroups = List(2, 1)
       val inputElements = List.range(0, 3)
       val expected = List(List(List(0, 1), List(2)), List(List(0, 2), List(1)), List(List(1, 2), List(0)))
-      group(inputGroups, inputElements) === expected
+      toNestedSet(group(inputGroups, inputElements)) === toNestedSet(expected)
     }
   }
 }
