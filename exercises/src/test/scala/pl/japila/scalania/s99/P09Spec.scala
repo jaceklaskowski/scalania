01e8617205dc5d3539a060cfcff704c6cd606e22
Jacek Laskowski <jacek@japila.pl>
2014-03-17 23:20:10 +0100

    Typo

--- exercises/src/test/scala/pl/japila/scalania/s99/P09Spec.scala
+++ exercises/src/test/scala/pl/japila/scalania/s99/P09Spec.scala
@@ -4,7 +4,7 @@ import org.specs2.mutable._
 import S99_P09.pack
 
 class P09Spec extends Specification {
-  "P07 solution" should {
+  "P09 solution" should {
     "Pack consecutive duplicates of list elements into sublists." in {
       val actual = pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
       val expected = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
