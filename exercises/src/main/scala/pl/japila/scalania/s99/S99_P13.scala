2a51677fbbb0f3f8c4189cc57d1f0dc0bc0a2d25
Jacek Laskowski <jacek@japila.pl>
2014-03-20 01:47:20 +0100

    scaladoc (copied from S-99)

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P13.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P13.scala
@@ -1,5 +1,12 @@
 package pl.japila.scalania.s99
 
 object S99_P13 {
+  /**
+   * Implement the so-called run-length encoding data compression method directly, i.e.
+   * don't use other methods you've written (like P09's pack); do all the work directly.
+   *
+   * scala> encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+   * res0: List[(Int, Symbol)] = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+   */
   def encodeDirect[T](ts: Seq[T]): Seq[(Int, T)] = ???
 }
