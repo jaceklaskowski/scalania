ab6f3c7a192df1ee4b5fdfef6a1002f89e20e978
Szymon Matejczyk <smatejczyk@gmail.com>
2014-06-04 18:08:54 +0200

    Third fix

--- exercises/src/main/scala/pl/japila/scalania/parallelCollections/Ex1ThirdElement.scala
+++ exercises/src/main/scala/pl/japila/scalania/parallelCollections/Ex1ThirdElement.scala
@@ -1,6 +1,7 @@
 package pl.japila.scalania.parallelCollections
 
 import scala.collection.parallel.ParSeq
+import scala.collection.immutable.TreeSet
 
 /**
  * Return an element that is the third maximal element in the collection.
@@ -11,7 +12,17 @@ import scala.collection.parallel.ParSeq
  */
 object Ex1ThirdElement {
   def third(col: Seq[Int]): Int = {
-    col.toSet.toList.sortBy((x: Int) => -x).apply(2)
+    var bestSet = TreeSet.empty[Int](Ordering[Int].reverse)
+    var worstInSet = Integer.MIN_VALUE
+    col.foreach {
+      el =>
+        if (el > worstInSet) {
+          bestSet += el
+        }
+        bestSet = bestSet.take(3)
+        worstInSet = bestSet.min
+    }
+    worstInSet
   }
 
   def third(col: ParSeq[Int]): Int = {
