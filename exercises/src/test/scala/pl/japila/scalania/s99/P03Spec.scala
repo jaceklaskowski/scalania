43a285d1104e27629da639b7e7f31385a9d1262c
Jacek Laskowski <jacek@japila.pl>
2014-06-07 14:39:38 +0200

    P03 got type-safer

--- exercises/src/test/scala/pl/japila/scalania/s99/P03Spec.scala
+++ exercises/src/test/scala/pl/japila/scalania/s99/P03Spec.scala
@@ -6,7 +6,12 @@ import S99_P03.nth
 class P03Spec extends Specification {
   "P03 solution" should {
     "Find the Kth element of a list." in {
-      nth(2, List(1, 1, 2, 3, 5, 8)) == 2
+      nth(-2, Nil) == None
+      nth(-2, Seq(1, 2, 3)) == None
+      nth(-1, Seq('a', 'b', 'c')) == None
+      nth(2, Nil) == None
+      nth(2, Seq(1)) == None
+      nth(2, Seq(1, 1, 2, 3, 5, 8)) == Some(2)
     }
   }
 }
