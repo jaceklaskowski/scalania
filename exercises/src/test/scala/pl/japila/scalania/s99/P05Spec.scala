3f235d88735539d07e0dd741d46ae3af61f70893
Jacek Laskowski <jacek@japila.pl>
2014-06-07 14:42:47 +0200

    Additional P05 tests

--- exercises/src/test/scala/pl/japila/scalania/s99/P05Spec.scala
+++ exercises/src/test/scala/pl/japila/scalania/s99/P05Spec.scala
@@ -6,7 +6,9 @@ import S99_P05.reverse
 class P05Spec extends Specification {
   "P05 solution" should {
     "Reverse a list." in {
-      reverse(List(1, 1, 2, 3, 5, 8)) === List(8, 5, 3, 2, 1, 1)
+      reverse(Seq(1, 1, 2, 3, 5, 8)) === Seq(8, 5, 3, 2, 1, 1)
+      reverse(Nil) === Nil
+      reverse(Seq(1)) === Seq(1)
     }
   }
 }
