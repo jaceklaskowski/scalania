d2e02684bc60d70d2767508cac7ff4fadee98847
Jacek Laskowski <jacek@japila.pl>
2014-06-07 12:53:48 +0200

    P01 got type-safer

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P01.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P01.scala
@@ -1,5 +1,5 @@
 package pl.japila.scalania.s99
 
 object S99_P01 {
-  def last[T](ts: List[T]): T = ???
+  def last[T](ts: Seq[T]): Option[T] = ???
 }
--- exercises/src/test/scala/pl/japila/scalania/s99/P01Spec.scala
+++ exercises/src/test/scala/pl/japila/scalania/s99/P01Spec.scala
@@ -6,7 +6,9 @@ import S99_P01.last
 class P01Spec extends Specification {
   "P01 solution" should {
     "Find the last element of a list" in {
-      last(List(1, 1, 2, 3, 5, 8)) == 8
+      last(Nil) == None
+      last(Seq(1)) == Some(1)
+      last(Seq(1, 1, 2, 3, 5, 8)) == Some(8)
     }
   }
 }
