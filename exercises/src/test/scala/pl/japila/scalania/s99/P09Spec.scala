9fd7670e2053d86294102cc8ec4b630963c5cc28
Jacek Laskowski <jacek@japila.pl>
2014-06-08 13:11:12 +0200

    More tests for P09

--- exercises/src/test/scala/pl/japila/scalania/s99/P09Spec.scala
+++ exercises/src/test/scala/pl/japila/scalania/s99/P09Spec.scala
@@ -6,12 +6,15 @@ import S99_P09.pack
 class P09Spec extends Specification {
   "P09 solution" should {
     "Pack consecutive duplicates of list elements into sublists." in {
-      val actual = pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
-      val expected = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+      val actual = pack(Seq('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+      val expected = Seq(Seq('a, 'a, 'a, 'a), Seq('b), Seq('c, 'c), Seq('a, 'a), Seq('d), Seq('e, 'e, 'e, 'e))
       actual === expected
     }
     "Applied to an empty list return an empty list" in {
       pack(Nil) mustEqual Nil
+    }
+    "Pack duplicates in a all-same-element list" in {
+      pack(Seq(1, 1, 1)) mustEqual Seq(Seq(1, 1, 1))
     }
   }
 }
