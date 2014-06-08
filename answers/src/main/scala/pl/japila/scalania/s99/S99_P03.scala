00c32f0f7859b24bbd9ab4d5a8c5eb30ced718d7
Jacek Laskowski <jacek@japila.pl>
2014-06-08 13:12:07 +0200

    Small change re shadowing

--- answers/src/main/scala/pl/japila/scalania/s99/S99_P03.scala
+++ answers/src/main/scala/pl/japila/scalania/s99/S99_P03.scala
@@ -3,7 +3,7 @@ package pl.japila.scalania.s99
 object S99_P03 {
   def nth[A](n: Int, l: List[A]): A = (n, l) match {
     case (0, h :: _) => h
-    case (n, _ :: tail) => nth(n - 1, tail)
+    case (_, _ :: tail) => nth(n - 1, tail)
     case (_, Nil) => throw new NoSuchElementException
   }
 }
