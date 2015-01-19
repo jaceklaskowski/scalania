5209992935c8c9f4b0fe156b33a17878420767de
Dominik <dominik223@o2.pl>
2015-01-19 21:31:16 +0100

    Added solution for collection/P04

--- exercises/src/main/scala/pl/japila/scalania/collection/P04.scala
+++ exercises/src/main/scala/pl/japila/scalania/collection/P04.scala
@@ -1,5 +1,19 @@
 package pl.japila.scalania.collection
 
 object P04 {
-  def groupIndexed[T](m: Map[T, Int]): Map[T, Seq[Int]] = ???
+  def groupIndexed[T](m: Seq[(T, Int)]): Map[T, Seq[Int]] = {
+    def numToIndices(num: Int): Seq[Int] = {
+      if (num > 1) 0 +: numToIndices(num - 1)
+      else Seq(1)
+    }
+
+    def mapToIndices[T](lastProcced: Int, s: Seq[Int]): Seq[Int] = {
+      s match {
+        case head +: tail => numToIndices(head - lastProcced) ++ mapToIndices(head, tail)
+        case Seq() => s
+      }
+    }
+
+    m.groupBy(_._1).mapValues(_.map(_._2)).mapValues(xs => mapToIndices(0, xs.sorted))
+  }
 }
