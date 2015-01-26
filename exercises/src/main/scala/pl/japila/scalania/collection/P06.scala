8030cca72776bda2409d8607cdbd201300729e5e
Radek Baranowski <baranowski.radek@gmail.com>
2015-01-26 18:05:49 +0100

    Update P06.scala
    my humble attempt :)

--- exercises/src/main/scala/pl/japila/scalania/collection/P06.scala
+++ exercises/src/main/scala/pl/japila/scalania/collection/P06.scala
@@ -3,6 +3,9 @@ package pl.japila.scalania.collection
 /**
  * Inspired by http://stackoverflow.com/q/28134729/1305344
  */
+ 
+ /** radek: tried to make it a true one-liner but guess I'm too slim in Scala for that :) */
+ 
 object P06 {
-  def isNumeric(as: Iterable[String]): Boolean = ???
+  def isNumeric(as: Iterable[String]): Boolean = {var out:Boolean=true;x foreach ("""^\d*$""".r findFirstIn _ getOrElse {out=false});out}
 }
