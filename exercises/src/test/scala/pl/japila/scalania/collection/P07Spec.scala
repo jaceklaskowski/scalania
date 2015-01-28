e4d681686ee56aabc3b627756c51e89e1a6d42ff
Dominik <dominik223@o2.pl>
2015-01-28 23:56:23 +0100

    Test for collection.P07
    Closes #40 #37. Thanks Dominik!

--- exercises/src/test/scala/pl/japila/scalania/collection/P07Spec.scala
+++ exercises/src/test/scala/pl/japila/scalania/collection/P07Spec.scala
@@ -1,14 +1,35 @@
 package pl.japila.scalania.collection
 
-import org.specs2.mutable.Specification
+import org.scalacheck.Prop
+import org.specs2.{ ScalaCheck, mutable }
+import org.scalacheck.Gen.{ choose, listOfN }
 import P07.sumOfPrecedingElements
 
-class P07Spec extends Specification {
+class P07Spec extends mutable.Specification with ScalaCheck {
+
+  def subtractionOfFollowingElements(ns: Seq[Int]): Seq[Int] = {
+    ns match {
+      case Seq() => ns
+      case elem :: Nil => elem +: subtractionOfFollowingElements(Nil)
+      case head :: tail => (head - tail.head) +: subtractionOfFollowingElements(tail)
+    }
+  }
+
   "sumOfPrecedingElements" should {
     "Sum all preceding elements in a collection." in {
       {
-        val ns = Seq(1, 3, 4, 5, 2, 4)
-        sumOfPrecedingElements(ns) must_=== Seq(1, 4, 8, 13, 15, 19)
+        val MIN = 1
+        val MAX = 10
+        val seqGenerator = for {
+          seqSize <- choose(MIN, MAX)
+          sequences <- listOfN(seqSize, choose(1, 10))
+        } yield {
+          sequences
+        }
+        val p1 = Prop.forAll(seqGenerator) {
+          seq => subtractionOfFollowingElements(sumOfPrecedingElements(seq).reverse).reverse must_=== seq
+        }
+        check(p1)
       }
     }
   }
