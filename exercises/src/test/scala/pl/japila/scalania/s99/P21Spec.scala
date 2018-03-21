97837a9c938a40206392f3f7a7633fe1b5105d12
Santa Samantaray <ssamantaray@mdsol.com>
2018-03-21 13:12:42 +0530

    Fixed 2 commentss and introduce 1 more issue

--- exercises/src/test/scala/pl/japila/scalania/s99/P21Spec.scala
+++ exercises/src/test/scala/pl/japila/scalania/s99/P21Spec.scala
@@ -2,11 +2,13 @@ package pl.japila.scalania.s99
 
 import org.specs2.mutable._
 import pl.japila.scalania._
+//Fix the import standard
+import S99_P21._
 
 object P21Spec extends Specification with ExamplesBlock {
   "P21 solution" should {
     "Insert an element at a given position into a list." in {
-      import S99_P21._
+
       solutions.foreach {
         case (solution, insertAtImpl) =>
           solution >> {
